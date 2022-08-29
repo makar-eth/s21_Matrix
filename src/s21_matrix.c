53a030f20002d3a0dbe58b73c21f031b1a17dc4e
makar.eth <makar.ananyin@yandex.ru>
2022-08-29 20:14:40 +0300

    full lib without check and tests

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -24,8 +24,8 @@ int s21_create_matrix(int rows, int columns, matrix_t *result) {
 void s21_remove_matrix(matrix_t *A) {
   for (int i = 0; i < A->rows; i++) {
     free(A->matrix[i]);
-    free(A->matrix);
   }
+  free(A->matrix);
 }
 
 int s21_eq_matrix(matrix_t *A, matrix_t *B) {
@@ -104,5 +104,171 @@ int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
 
 int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
   int status = OK;
+  if (A->matrix != NULL && A != NULL) {
+    int rows = A->rows;
+    int columns = A->columns;
+    int create_status = s21_create_matrix(rows, columns, result);
+    if (create_status == 0) {
+      for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+          result->matrix[i][j] = number * A->matrix[i][j];
+        }
+      }
+    } else {
+      status = ERROR;
+    }
+  } else {
+    status = ERROR;
+  }
+  return status;
+}
+
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+  int status = OK;
+  if (A != NULL && B != NULL && A->matrix != NULL && B->matrix != NULL) {
+    if (A->columns == B->rows) {
+      status = s21_create_matrix(A->rows, B->columns, result);
+       if (status == 0) {
+        for (int i = 0; i < result->rows; i++) {
+          for (int j = 0; j < result->columns; j++) {
+            double sum = 0;
+            for (int k = 0; k < A->columns; k++) {
+              sum += A->matrix[i][k] * B->matrix[k][j];
+            }
+            result->matrix[i][j] = sum;
+          }
+        }
+      }
+    } else {
+      status = CALC_ERROR;
+    }
+  } else {
+    status = ERROR;
+  }
+  return status;
+}
+
+int s21_transpose(matrix_t *A, matrix_t *result) {
+  int status = OK;
+  if (A->matrix != NULL && A != NULL) {
+    int rows = A->rows;
+    int columns = A->columns;
+    int create_status = s21_create_matrix(columns, rows, result);
+    if (create_status == 0) {
+      for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+          result->matrix[j][i] = A->matrix[i][j];
+        }
+      }
+    } else {
+      status = ERROR;
+    }
+  } else {
+    status = ERROR;
+  }
+  return status;
+}
+
+int s21_calc_complements(matrix_t *A, matrix_t *result) {
+  int status = OK;
+  if (A != NULL && A->matrix != NULL) {
+    if (A->columns == A->rows) {
+      int grade = A->rows;
+      int create_status = s21_create_matrix(grade, grade, result);
+      if (create_status == 0) {
+        for (int i = 0; i < grade; i++) {
+          for (int j = 0; j < grade; j++) {
+            matrix_t *buf = NULL;
+            s21_create_matrix(grade - 1, grade - 1, buf);
+            s21_fill_minor(A, buf, i, j);
+            double *minor_determinant = NULL;
+            s21_determinant(buf, minor_determinant);
+            result->matrix[i][j] = *minor_determinant;
+            s21_remove_matrix(buf);
+          }
+        }
+        for (int i = 0; i < grade; i++) {
+          for (int j = 0; j < grade; j++) {
+            result->matrix[i][j] = result->matrix[i][j] * pow((-1), i + j);
+          }
+        }
+      } else {
+        status = ERROR;
+      }
+    } else {
+      status = CALC_ERROR;
+    }
+  } else {
+    status = ERROR;
+  }
+  return status;
+}
+
+int s21_determinant(matrix_t *A, double *result) {
+  int status = OK;
+  if (A != NULL && A->matrix != NULL) {
+    if (A->columns == A->rows) {
+      int grade = A->rows;
+      if (grade == 1) {
+        *result = A->matrix[0][0];
+      } else if (grade > 1) {
+        *result = 0;
+        for (int j = 0; j < grade; j++) {
+          matrix_t *buf = NULL;
+          s21_create_matrix(grade - 1, grade - 1, buf);
+          s21_fill_minor(A, buf, 0, j);
+          double *minor_determinant = NULL;
+          s21_determinant(buf, minor_determinant);
+          s21_remove_matrix(buf);
+          *result += pow(-1, j) * A->matrix[0][j] * (*minor_determinant);
+        }
+      }
+    } else {
+      status = CALC_ERROR;
+    }
+  } else {
+    status = ERROR;
+  }
+  return status;
+}
+
+int s21_fill_minor(matrix_t *A, matrix_t *B, int row, int column) {
+  int grade = A->rows;
+  int minor_column = 0;
+  int minor_row = 0;
+  for (int i = 0; i < grade; i++) {
+    for (int j = 0; j < grade; j++) {
+      if (i != row && j != column) {
+        if (minor_column == grade - 1) {
+          minor_row++;
+          minor_column = 0;
+        }
+        B->matrix[minor_row][minor_column] = A->matrix[i][j];
+        minor_column++;
+      }
+    }
+  }
+  return OK;
+}
+
+int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
+  int status = OK;
+  if (A->matrix != NULL && A != NULL) {
+    double *D = NULL;
+    if (s21_determinant(A, D) == 0 && *D != 0) {
+      matrix_t *buf = NULL;
+      status = s21_calc_complements(A, buf);
+      if (status == 0) {
+        status = s21_transpose(buf, result);
+        s21_remove_matrix(buf);
+      } else {
+        status = ERROR;
+      }
+    } else {
+      status = CALC_ERROR;
+    }
+  } else {
+    status = ERROR;
+  }
   return status;
 }
--- src/s21_matrix.h
+++ src/s21_matrix.h
@@ -26,3 +26,4 @@ int s21_transpose(matrix_t *A, matrix_t *result);
 int s21_calc_complements(matrix_t *A, matrix_t *result);
 int s21_determinant(matrix_t *A, double *result);
 int s21_inverse_matrix(matrix_t *A, matrix_t *result);
+int s21_fill_minor(matrix_t *A, matrix_t *B, int row, int column);
